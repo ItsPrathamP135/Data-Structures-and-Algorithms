6eb8bdb2df0abbc6269345dea46054c116f310dd
Pratham27 <prathampporeddiwar27@gmail.com>
2025-11-20 23:51:27 +0530

    Streak Maintain

--- src/com/problems/IslandTreasure.java
+++ src/com/problems/IslandTreasure.java
@@ -5,66 +5,63 @@ import java.util.Queue;
 
 public class IslandTreasure {
     public void islandsAndTreasure(int[][] grid) {
-	int iL = grid.length, jL = grid[0].length, INF = Integer.MAX_VALUE;;
-        int[][] directions = { {0, 1}, {1, 0}, {0,-1}, {-1,0}};
-        Queue<int[]> queue = new LinkedList<>();
-        
-        for(int i = 0; i < iL; i++) {
-            for(int j = 0; j < jL; j++) {
-        	if(grid[i][j] == 0) {
-        	    queue.add(new int[]{i, j});
-        	}
-            }
-        }
-        
-        while(!queue.isEmpty()) {
-            int[] list = queue.poll();
-            int iN, jN;
-            
-            for(int[] direct: directions) {
-        	iN = list[0] + direct[0];
-        	jN = list[1] + direct[1];
-        	
-        	if(iN >= 0 && iN < iL && jN>=0 && jN < jL && (grid[iN][jN] == INF || grid[iN][jN] > grid[list[0]][list[1]] + 1)) {
-        	    grid[iN][jN] = grid[list[0]][list[1]] + 1;
-        	    queue.add(new int[]{iN, jN});
-        	}
-            }
-            
-        }
-     }
-    
-    public static void printGrid(int[][] grid) {
-        for (int[] row : grid) {
-            for (int cell : row) {
-                if (cell == Integer.MAX_VALUE) {
-                    System.out.print("INF ");
-                } else {
-                    System.out.print(cell + " ");
-                }
-            }
-            System.out.println();
-        }
+	int iL = grid.length, jL = grid[0].length, INF = Integer.MAX_VALUE;
+	;
+	int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+	Queue<int[]> queue = new LinkedList<>();
+
+	for (int i = 0; i < iL; i++) {
+	    for (int j = 0; j < jL; j++) {
+		if (grid[i][j] == 0) {
+		    queue.add(new int[] { i, j });
+		}
+	    }
+	}
+
+	while (!queue.isEmpty()) {
+	    int[] list = queue.poll();
+	    int iN, jN;
+
+	    for (int[] direct : directions) {
+		iN = list[0] + direct[0];
+		jN = list[1] + direct[1];
+
+		if (iN >= 0 && iN < iL && jN >= 0 && jN < jL
+			&& (grid[iN][jN] == INF || grid[iN][jN] > grid[list[0]][list[1]] + 1)) {
+		    grid[iN][jN] = grid[list[0]][list[1]] + 1;
+		    queue.add(new int[] { iN, jN });
+		}
+	    }
+
+	}
     }
-    
+
+    public static void printGrid(int[][] grid) {
+	for (int[] row : grid) {
+	    for (int cell : row) {
+		if (cell == Integer.MAX_VALUE) {
+		    System.out.print("INF ");
+		} else {
+		    System.out.print(cell + " ");
+		}
+	    }
+	    System.out.println();
+	}
+    }
+
     public static void main(String[] args) {
-        IslandTreasure solution = new IslandTreasure();
-        int INF = Integer.MAX_VALUE;
+	IslandTreasure solution = new IslandTreasure();
+	int INF = Integer.MAX_VALUE;
 
-        // Example test case
-        int[][] grid = {
-            {INF, -1, 0, INF},
-            {INF, INF, INF, -1},
-            {INF, -1, INF, -1},
-            {0, -1, INF, INF}
-        };
+	// Example test case
+	int[][] grid = { { INF, -1, 0, INF }, { INF, INF, INF, -1 }, { INF, -1, INF, -1 }, { 0, -1, INF, INF } };
 
-        System.out.println("Original Grid:");
-        printGrid(grid);
+	System.out.println("Original Grid:");
+	printGrid(grid);
 
-        solution.islandsAndTreasure(grid);
+	solution.islandsAndTreasure(grid);
 
-        System.out.println("\nModified Grid:");
-        printGrid(grid);
+	System.out.println("\nModified Grid:");
+	printGrid(grid);
     }
 }
--- src/com/problems/LargestRectangle.java
+++ src/com/problems/LargestRectangle.java
@@ -5,36 +5,35 @@ import java.util.Arrays;
 // Different Method is also available
 
 public class LargestRectangle {
-	
-	public static int getAreaOfRectangle(int[] arr) {
-		int area = 0, min = 0;
-		
-		for(int i = 1; i < arr.length; i++) {
-			for(int j = 0; j < arr.length-i; j++) {
-				min = getMin(Arrays.copyOfRange(arr, j, j+i));
-				if((min * i) > area) {
-					area = min * i;
-				}
-			}
-		}
-		return area;
-	}
-	
-	public static int getMin(int[] arr) {
-		int min = Integer.MAX_VALUE;	
-		for(int i = 0; i < arr.length; i++) {
-			if(min > arr[i]) {
-				min = arr[i];
-			}
-		}
-		
-		return min;
-	}
-	
 
-	public static void main(String[] args) {
-		int[] arr = {2, 1, 5, 6, 2, 3};
-		System.out.println(getAreaOfRectangle(arr));
+    public static int getAreaOfRectangle(int[] arr) {
+	int area = 0, min = 0;
+
+	for (int i = 1; i < arr.length; i++) {
+	    for (int j = 0; j < arr.length - i; j++) {
+		min = getMin(Arrays.copyOfRange(arr, j, j + i));
+		if ((min * i) > area) {
+		    area = min * i;
+		}
+	    }
 	}
+	return area;
+    }
+
+    public static int getMin(int[] arr) {
+	int min = Integer.MAX_VALUE;
+	for (int i = 0; i < arr.length; i++) {
+	    if (min > arr[i]) {
+		min = arr[i];
+	    }
+	}
+
+	return min;
+    }
+
+    public static void main(String[] args) {
+	int[] arr = { 2, 1, 5, 6, 2, 3 };
+	System.out.println(getAreaOfRectangle(arr));
+    }
 
 }
